85a79981dbb2344389f8ed3661462693abe07161
Ali Sattari <ali.sattari@gmail.com>
2024-12-12 16:34:35 +0100

    day 12 p1 working

--- 12/main.py
+++ 12/main.py
@@ -1,4 +1,3 @@
-from tarfile import itn
 from typing import List, Optional, Tuple
 from pprint import pprint
 from math import floor
@@ -6,74 +5,35 @@ from itertools import repeat
 from collections import defaultdict
 import time
 
-class Plant:
-    def __init__(self, name: str):
-        self.name = name
-        self.count = 0
-        self.regions = []
-        self.reg_count = 0
-        self.points = []
-        self.dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+class Pos:
+    def __init__(self, y:int, x:int):
+        self.y = y
+        self.x = x
 
     def __repr__(self):
-        return self.name
+        return f"Pos({self.y}, {self.x})"
 
-    def add_pos(self, pos:tuple):
-        self.points.append(pos)
+    def __add__(self, other):
+        return Pos(self.y + other.y, self.x + other.x)
 
-    def define_regions(self):
-        for pos in self.points:
-            r = self.get_region(pos)
+    def __sub__(self, other):
+        return Pos(self.y - other.y, self.x - other.x)
 
-            if r == -1:
-                self.regions.append([pos])
-                self.reg_count += 1
-            else:
-                self.regions[r].append(pos)
+    def __eq__(self, other):
+        return isinstance(other, Pos) and (self.y, self.x) == (other.y, other.x)
 
-    def get_region(self, pos: tuple, visited=[]) -> int:
-        reg = -1
-        # for first pos per plant
-        if len(self.regions) == 0:
-            return reg
+    def __hash__(self):
+        return hash((self.y, self.x))
 
-        to_check = []
-        for d in self.dirs:
-            c = add_tuples(pos, d)
-            # if pos has any neighbor in points
-            if c in self.points and c not in visited:
-                # check existing regions
-                for i, r in enumerate(self.regions):
-                    if c in r:
-                        reg = i
-                    else:
-                        to_check.append(c)
+    def __lt__(self, other):
+        return (self.y, self.x) < (other.y, other.x)
 
-        if reg == -1:
-            for c in to_check:
-                # recurse on c to find region
-                return self.get_region(c, visited + [c])
+    def is_inbounds(self, h: int, w: int):
+        return 0 <= self.y < h and 0 <= self.x < w
 
-        return reg
-
-    def price(self) -> int:
-        p = 0
-        for r in self.regions:
-            p += self.perimeter(r) * self.area(r)
-        return p
-
-    def perimeter(self, region: list) -> int:
-        walls = 0
-        for p in region:
-            for d in self.dirs:
-                c = add_tuples(p, d)
-                if c not in region:
-                    walls += 1
-
-        return walls
-
-    def area(self, region: list) -> int:
-        return len(region)
+    def get_buren(self):
+        for delta in (Pos(0, 1), Pos(0, -1), Pos(1, 0), Pos(-1, 0)):
+            yield self + delta
 
 class Solution:
     def __init__(self, grid: List):
@@ -81,25 +41,55 @@ class Solution:
         self.grid = self.map_grid(grid)
         self.height = len(grid)
         self.width = len(grid[0])
-        self.regions = defaultdict(list)
 
-    def map_grid(self, grid: List):
+    def map_grid(self, grid: List) -> dict[Pos, str]:
+        m = dict()
         for y, row in enumerate(grid):
             for x, p in enumerate(row):
-                if p not in self.plants:
-                    self.plants[p] = Plant(p)
-                self.plants[p].add_pos((y,x))
+                m[Pos(y,x)] = p
+        return m
 
-    def calculate1(self) -> int:
+    def calculate1_dfs(self) -> int:
         total = 0
-        for plant in self.plants.values():
-            plant.define_regions()
-            price = plant.price()
+
+        cells = set(self.grid.keys())
+        while cells:
+            pos = cells.pop()
+            plant = self.grid[pos]
+            region = self.find_region(pos)
+            price = len(region) * self.perimeter(region)
+            print(f"Plant {plant} with region of {len(region)} = {price}", "\t"*3, pos)
+
+            cells -= region
             total += price
-            print(plant, price, plant.reg_count)
-            # pprint(plant.regions)
-            # print("-"*50)
+
         return total
+
+    def find_region(self, pos: Pos) -> set[Pos]:
+        q: List[Pos] = [pos]
+        region: set[Pos] = {pos}
+        plant = self.grid[pos]
+
+        while q:
+            current = q.pop()
+            for n in current.get_buren():
+                if not n.is_inbounds(self.height, self.width):
+                    continue
+
+                if n not in region and self.grid[n] == plant:
+                    q.append(n)
+                    region.add(n)
+
+        return region
+
+    def perimeter(self, region: set[Pos]) -> int:
+        t = 0
+        for p in region:
+            t += 4 - len(set(p.get_buren()) & region)
+
+        return t
+
+
 
 def add_tuples(t1: tuple[int, int], t2: tuple[int, int]) -> tuple[int, int]:
     return tuple(a + b for a, b in zip(t1, t2))
@@ -113,21 +103,21 @@ def input_to_list(f: str) -> List:
 
     return stuff
 
-
 if __name__ == "__main__":
-    # test_input = input_to_list("./test-input")
-    # test_answer1 = 1930 # 11 regions
-    # inst = Solution(test_input)
-    # ans1 = inst.calculate1()
-    # # ans2 = inst.calculate2(7)
-    # print("Part1:", ans1, ans1 == test_answer1)
-    # # print("Part2:", ans2, ans2 == test_answer1)
+    test_input = input_to_list("./test-input")
+    test_answer1 = 1930 # 11 regions
+    test_answer2 = 1206 # 11 regions
+    inst = Solution(test_input)
+    ans1 = inst.calculate1_dfs()
+    # ans2 = inst.calculate2(7)
+    print("Part1:", ans1, ans1 == test_answer1)
+    # print("Part2:", ans2, ans2 == test_answer1)
 
-    input = input_to_list("./input")
-    inst = Solution(input)
-    ans1 = inst.calculate1()
-    # ans2 = inst.calculate2(75)
-    print("Part 1:", ans1)
-    # print("Part 2:", ans2)
+    # input = input_to_list("./input")
+    # inst = Solution(input)
+    # ans1 = inst.calculate1_dfs()
+    # # ans2 = inst.calculate2(75)
+    # print("Part 1:", ans1)
+    # # print("Part 2:", ans2)
 
-    # p1: 1215102 too low
+    # # p1: 1464678
