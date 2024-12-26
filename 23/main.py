34fe4482e4d4038ce5b43bb6f50a79e9b430bcbe
Ali Sattari <ali.sattari@gmail.com>
2024-12-26 21:01:57 +0100

    some graph sorsery (Bron-K?) right there!

--- 23/main.py
+++ 23/main.py
@@ -15,11 +15,33 @@ class Solution:
 
         return len(self.groups)
 
+    def calculate2(self) -> str:
+        # pprint(self.network)
+
+        res = []
+        self.find_many_mates([], set(self.network.keys()), set(), res)
+        largest_group = sorted(res, reverse=True, key=len)[0]
+
+        return ','.join(sorted(largest_group))
+
     def form_groups(self):
         for src, dsts in self.network.items():
             if src.startswith('t'):
                 g = self.find_mates(src, dsts)
                 self.groups.update(g)
+
+    def find_many_mates(self, mates: list, candidates: set, visited: set, res=[]):
+        if not candidates and not visited:
+            return mates
+
+        for c in list(candidates):
+            t = self.find_many_mates(mates + [c], candidates & self.network[c], visited & self.network[c], res)
+            if len(t) > 1:
+                res.append(t)
+            candidates.remove(c)
+            visited.add(c)
+
+        return []
 
     def find_mates(self, src: str, pairs: set) -> List[str]:
         pl = list(pairs)
@@ -46,18 +68,16 @@ def process_input(f: str):
 if __name__ == "__main__":
     # test_input = process_input("./test-input")
     # test_answer1 = 7
+    # test_answer2 = 'co,de,ka,ta'
     # inst = Solution(test_input)
-    # answ1 = inst.calculate()
-    # print("Part1:", answ1 ,answ1 == test_answer1)
-    # # answ2 = inst.calculate(25)
-    # # print("Part2:", answ2)
+    # # answ1 = inst.calculate()
+    # # print("Part1:", answ1 ,answ1 == test_answer1)
+    # answ2 = inst.calculate2()
+    # print("Part2:", answ2, answ2 == test_answer2)
 
     input = process_input("./input")
     inst = Solution(input)
     answ1 = inst.calculate()
     print("Part 1:", answ1)
-    # answ2 = inst.calculate(25)
-    # print("Part 2:", answ2)
-
-    # p1: 1370
-    # p2:
+    answ2 = inst.calculate2()
+    print("Part 2:", answ2)
